3ac3c6b563ac2b07144a6a96cbaa86cf8ac2f3fe
Thien phuc <thienphuc484@gmail.com>
2025-10-18 12:50:37 +0700

    Init commit

---
+++ Filetag/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Filetag.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Filetag/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Filetag.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
